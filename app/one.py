e4bd2e1d54b08d99a31299ff6bcf12791322ab8b
yashpandav <pandavyash076@gmail.com>
2025-05-29 11:57:17 +0530

    deploy

--- app/one.py
+++ app/one.py
@@ -6,7 +6,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 def generate_query_embedding(query):
         print("Processing query...")
         query = genai.embed_content(
-                model="text-embedding-004",
+                model="models/text-embedding-004",
                 content=query,
                 task_type="retrieval_query"
         )
