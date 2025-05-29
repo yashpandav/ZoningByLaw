f9c0d60cb0f1f9da941b3dc6530442ad2965b20b
yashpandav <pandavyash076@gmail.com>
2025-05-29 11:57:11 +0530

    deplot

--- app/one.py
+++ app/one.py
@@ -1,12 +1,13 @@
-from google import genai
 import os
+import google.generativeai as genai
 
-client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def generate_query_embedding(query):
-        query = client.models.embed_content(
+        print("Processing query...")
+        query = genai.embed_content(
                 model="text-embedding-004",
-                contents=query,
+                content=query,
                 task_type="retrieval_query"
         )
-        return query.embeddings[0].values
+        return query["embedding"]
